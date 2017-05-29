53765c90342ef52dec88593bf64abf1c7a2493a1
Raghu Kaippully <rkaippully@gmail.com>
2017-05-30 00:05:00 +0530

    Loading COFF kernel image

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,19 @@
-void main()
+
+int i = 10;
+
+static char c[10];
+
+int main1()
 {
-  for (;;);
+  c[0] = 10;
+  return c[2];
+}
+
+int main()
+{
+  unsigned short* ptr = (unsigned short*)0xbffb8000;
+  for (int i = 0; i < 80*25 - 1; i++)
+    *ptr++ = 0x7145;
+
+  for(;;);
 }
