a06493799d091c8190553f27b48a08fe9e26707e
Raghu Kaippully <rkaippully@gmail.com>
2017-05-30 09:20:58 +0530

    Set up stack

---
+++ src/include/stdint.h
@@ -0,0 +1,18 @@
+/*
+  Trantor Operating System
+
+  Copyright (C) 2017 Raghu Kaippully
+*/
+
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+
+typedef signed char  int8_t;
+typedef signed short int16_t;
+typedef signed int   int32_t;
+
+#endif
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,16 +1,30 @@
+/*
+  Trantor Operating System
+
+  Copyright (C) 2017 Raghu Kaippully
+*/
 
 int i = 10;
 
-static char c[10];
+#include "stdint.h"
 
-int main1()
-{
-  c[0] = 10;
-  return c[2];
-}
+static uint8_t kernel_stack[4096];
+
+// This cannot be a function call because stack is not yet setup :)
+#define init_stack() \
+  {                                                \
+    uint16_t x;                                    \
+    __asm__ volatile("movw %%ds, %0  \n"           \
+                     "movw %0, %%ss   \n"          \
+                     "movl %1, %%esp  \n"          \
+                     : "=q" (x)                    \
+                     : "i" (&kernel_stack[4096])); \
+  }
 
 int main()
 {
+  init_stack();
+
   unsigned short* ptr = (unsigned short*)0xbffb8000;
   for (int i = 0; i < 80*25 - 1; i++)
     *ptr++ = 0x7145;
